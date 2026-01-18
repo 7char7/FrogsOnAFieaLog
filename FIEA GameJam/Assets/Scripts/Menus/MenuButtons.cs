51c188c9cb3028f2a52e1ed875629d1c8a60771b
kingkaue <kaueking@gmail.com>
2026-01-18 03:58:35 -0500

    how to play and controls page to main menu

--- FIEA GameJam/Assets/Scripts/Menus/MenuButtons.cs
+++ FIEA GameJam/Assets/Scripts/Menus/MenuButtons.cs
@@ -1,52 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
-   public void LoadMainMenu()
-   {
-       if (SceneFadeManager.Instance != null)
-       {
-           SceneFadeManager.Instance.FadeToScene("MainMenu", true);
-       }
-       else
-       {
-           SceneManager.LoadScene("MainMenu");
-       }
-   }
+    [SerializeField] private GameObject howToPlayPanel;
+    [SerializeField] private GameObject controlsPanel;
+    public void LoadMainMenu()
+    {
+        if (SceneFadeManager.Instance != null)
+        {
+            SceneFadeManager.Instance.FadeToScene("MainMenu", true);
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
 
-   public void LoadShop()
-   {
-       SceneManager.LoadScene("Shop");
-   }
+    public void LoadShop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
 
-   public void LoadSummary()
-   {
-       if (SceneFadeManager.Instance != null)
-       {
-           SceneFadeManager.Instance.FadeToScene("Summary", true);
-       }
-       else
-       {
-           SceneManager.LoadScene("Summary");
-       }
-   }
+    public void ToggleHowToPlay()
+    {
+        if (howToPlayPanel != null)
+        {
+            if (howToPlayPanel.activeSelf)
+            {
+                howToPlayPanel.SetActive(false);
+                return;
+            }
+            else
+            {
+                howToPlayPanel.SetActive(true);
+                return;
+            }
+        }
+    }
 
-   public void LoadFakePlaying()
-   {
-       if (SceneFadeManager.Instance != null)
-       {
-           SceneFadeManager.Instance.FadeToScene("FakePlaying", false);
-       }
-       else
-       {
-           SceneManager.LoadScene("FakePlaying");
-       }
-   }
+    public void ToggleControls()
+    {
+        if (controlsPanel != null)
+        {
+            if (controlsPanel.activeSelf)
+            {
+                controlsPanel.SetActive(false);
+                return;
+            }
+            else
+            {
+                controlsPanel.SetActive(true);
+                return;
+            }
+        }
+    }
 
-   public void Quit()
-   {
-       Application.Quit();
-   }
+    public void Quit()
+    {
+        Application.Quit();
+    }
 
 }
